25864a4981ba165470f078fb9778c7c841c26831
Julian van Doorn <jvdoorn@antarc.com>
2021-10-28 19:21:17 +0200

    Add docstrings and add static analyse method to BaseScheme.

--- measure/g2.py
+++ measure/g2.py
@@ -1,16 +1,17 @@
 import time
 
+import numpy as np
+from matplotlib import pyplot as plt
+
 from interface import DELAY_LINES
-from .scheme import Scheme
+from .scheme import BaseScheme
 
 
-class G2(Scheme):
+class G2(BaseScheme):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, data_shape=(4, 5), iterations=5, **kwargs)
 
-        self.counts1 = self.data[1]
-        self.counts2 = self.data[2]
-        self.coincidences = self.data[3]
+        self.data[0] = np.arange(5)
 
     def setup(self):
         # Set the interferometer rotation to a known state.
@@ -22,10 +23,24 @@ class G2(Scheme):
 
     def iteration(self, i):
         # Set the desired state
-        self.coincidence_circuit.set_delay(i, 'WA')
-        self.coincidence_circuit.set_delay(i, 'CA')
+        self.coincidence_circuit.set_delay(self.data[0, i], 'WA')
+        self.coincidence_circuit.set_delay(self.data[0, i], 'CA')
         self.coincidence_circuit.clear_counters()
         # Wait for the data to be acquired.
         time.sleep(0.5)
         # Obtain the data
-        self.counts1[i], self.counts2[i], self.coincidences[i] = self.coincidence_circuit.save_and_read_counts()
+        self.data[1:, i] = self.coincidence_circuit.save_and_read_counts()
+
+    @staticmethod
+    def analyse(data):
+        delays = data[0]
+        counts1 = data[1]
+        counts2 = data[2]
+        coincidences = data[3]
+
+        g2 = coincidences / (counts1 * counts2)
+
+        plt.plot(delays, g2)
+        plt.ylabel('g2')
+        plt.xlabel('delay')
+        plt.show()
--- measure/scheme.py
+++ measure/scheme.py
@@ -12,7 +12,13 @@ from interface import CoincidenceCircuit, Interferometer
 DATETIME_FORMAT = '%Y-%m-%d-%H:%M:%S'
 
 
-class Scheme(ABC):
+class BaseScheme(ABC):
+    """
+    Provides a template for measurement schemes following the template method behavioural pattern. It takes care of
+    setting up the serial interfaces and closing them when we are done. It furthermore takes care of saving and
+    compressing data (along with metadata).
+    """
+
     def __init__(self, coincidence_circuit: CoincidenceCircuit, interferometer: Interferometer,
                  data_shape: Tuple[int, ...], iterations: int):
         self.coincidence_circuit = coincidence_circuit
@@ -25,6 +31,11 @@ class Scheme(ABC):
 
     @property
     def metadata(self) -> dict:
+        """
+        Dictionary with metadata that is saved alongside the compressed data files. You can extend this method to add
+        additional data. You should return `super().metadata.update(d)` where `d` is your dictionary.
+        :return: a dictionary with metadata.
+        """
         return {
             "scheme":    self.scheme_name,
             "timestamp": self.timestamp,
@@ -32,26 +43,46 @@ class Scheme(ABC):
 
     @property
     def scheme_name(self) -> str:
+        """
+        Short hand to get the name of the measurement scheme.
+        """
         return self.__class__.__name__
 
     @property
     def timestamp(self) -> str:
+        """
+        Returns a formatted datetime of when the measurement scheme was run.
+        """
         return self._timestamp.strftime(DATETIME_FORMAT)
 
     @timestamp.setter
     def timestamp(self, value: datetime):
+        """
+        Updates the timestamp of when the measurement started running.
+        """
         self._timestamp = value
 
     @property
     def data_folder(self) -> str:
+        """
+        The folder where the data will be stored in.
+        """
         return f"data/{self.scheme_name}"
 
     @property
     def file_name(self) -> str:
+        """
+        The folder the data will be stored in including the file name.
+        """
         return f"{self.data_folder}/{self.timestamp}.npz"
 
     @final
-    def run(self) -> np.ndarray:
+    def __call__(self):
+        """
+        Runs the whole measurement scheme. It will prepare the scheme and do any required setup. It will then iterate,
+        acquiring data and finally save that data. Additionally it will run the cleanup and return the acquired data.
+        :return:
+        """
         # Prepares the system.
         self.prepare()
         # Runs code that is required once.
@@ -60,18 +91,22 @@ class Scheme(ABC):
         # Gives the Arduinos time to get settled.
         sleep(1)
         # Run the actual measurements.
+        logger.info(f"Starting measurements for {self.scheme_name}.")
         for i in range(self.iterations):
             self.iteration(i)
-
+        logger.info(f"Finished measurements for {self.scheme_name}.")
         # Save all data.
         self.save()
         # Perform any cleanup.
-        self.teardown()
+        self.cleanup()
         # Return the acquired data.
         return self.data
 
     @final
-    def prepare(self):
+    def prepare(self) -> None:
+        """
+        Prepares the system, initializes serial connections and creates the data folder if it does not exist.
+        """
         logger.info(f"Preparing {self.scheme_name} measurement scheme...")
         self.timestamp = datetime.now()
 
@@ -83,20 +118,47 @@ class Scheme(ABC):
         self.interferometer.__enter__()
 
     @abstractmethod
-    def setup(self):
+    def setup(self) -> None:
+        """
+        This method is run once. It should be used to set the system to a known state, par example by setting the delay
+        lines to their initial values.
+        """
         pass
 
     @abstractmethod
-    def iteration(self, i):
+    def iteration(self, i: int) -> None:
+        """
+        This method is called repeatedly. It should be used as the main way of updating the system and then acquiring
+        data from it.
+        :param i: the iteration number.
+        """
         pass
 
     @final
-    def save(self):
+    def save(self) -> None:
+        """
+        Saves the acquired data, along with metadata, to file and compresses it.
+        """
         logger.info(f"Saving data to file: {self.file_name}!")
         np.savez_compressed(self.file_name, data=self.data, **self.metadata)
 
     @final
-    def teardown(self):
+    def cleanup(self) -> None:
+        """
+        Closes the serial connections.
+        """
         logger.info(f"Tearing down {self.scheme_name} measurement scheme...")
         self.coincidence_circuit.__exit__()
         self.interferometer.__exit__()
+
+    @staticmethod
+    def analyse(data: np.ndarray) -> None:
+        """
+        This method can be called to analyse the acquired data. It is not run automatically. The rational for making it
+        static is to signal that it is not necessarily part of the scheme and does not need to be used. It is part of
+        the scheme to make it easier to swap scheme without having to think about specifying a new `analyse` method
+        somehow. Additionally, since it is static it can be called without creating an object thus allowing it to be run
+        without needing to instantiate serial connections.
+        :param data: the data as acquired by running the scheme.
+        """
+        pass
