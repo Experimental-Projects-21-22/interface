94eae039368996ba88d41d40ad20bbbee1a848c8
DouweRem <remmeltsdouwe@gmail.com>
2022-01-18 12:36:30 +0100

    Dynamic measure time in GUI rates.

--- measure/schemes/gui_rates.py
+++ measure/schemes/gui_rates.py
@@ -3,13 +3,15 @@ import random
 # from loguru import logger
 from interface import CoincidenceCircuit
 from utils.delays import DelayLines
+import time
 
 # Get around the max recursion depth
 import sys
+
 sys.setrecursionlimit(100000)
 
 # tkinter stuff
-font = ("Courier", 44)
+font = ("Courier", 70)
 root = tk.Tk()
 tk.Label(root, text='Counter 1', font=font).grid(row=0, column=0)
 tk.Label(root, text='Counter 2', font=font).grid(row=1, column=0)
@@ -26,11 +28,15 @@ lab_coinc.grid(row=2, column=1)
 WINDOW_SIZE = 12
 fixed_delay = 24
 CORRECTION_FACTOR = 0
+MEASURE_TIME = 5
 
-CA_steps = DelayLines.CA.calculate_steps(fixed_delay) + CORRECTION_FACTOR
-WA_steps = DelayLines.WA.calculate_steps(fixed_delay + WINDOW_SIZE) + CORRECTION_FACTOR
-CB_steps = DelayLines.CB.calculate_steps(fixed_delay)
-WB_steps = DelayLines.WB.calculate_steps(fixed_delay + WINDOW_SIZE)
+# CA_steps = DelayLines.CA.calculate_steps(fixed_delay) + CORRECTION_FACTOR
+CA_steps = 37
+# WA_steps = DelayLines.WA.calculate_steps(fixed_delay + WINDOW_SIZE) + CORRECTION_FACTOR
+WA_steps = 86
+# CB_steps = DelayLines.CB.calculate_steps(fixed_delay)
+CB_steps = 29
+WB_steps = 76
 
 # Load circuit
 coincidence_circuit = CoincidenceCircuit(baudrate=115200, port='/dev/tty.usbmodem142101')
@@ -47,21 +53,22 @@ def measure_rate():
     # Function that continuously measures the rate
 
     # Measure for 2 seconds
-    measurement = coincidence_circuit.measure(1)
+    measurement = coincidence_circuit.measure(MEASURE_TIME)
     # measurement = (random.random() * 1e6, random.random() * 1e6, random.random() * 1e6)
     # Split the measurements and convert to Hz
-    counts_1 = f'{measurement[0]:.2E}'
-    counts_2 = f'{measurement[1]:.2E}'
-    counts_coinc = f'{measurement[2]:.2E}'
+    counts_1 = f'{measurement[0] / MEASURE_TIME:.2E} /s'
+    counts_2 = f'{measurement[1] / MEASURE_TIME:.2E} /s'
+    counts_coinc = f'{measurement[2] / MEASURE_TIME:.2E} /s'
 
     # Update the labels
     lab_1.config(text=counts_1)
     lab_2.config(text=counts_2)
     lab_coinc.config(text=counts_coinc)
 
-    root.after(1000, measure_rate) # Runs itself again after 1000 milliseconds
+    root.after(1000, measure_rate)  # Runs itself again after 1000 milliseconds
 
 
+time.sleep(1)
 # start the loop
 measure_rate()
 
