5439a1fc8664a3da0901f719ce4a5f61ef283838
Julian van Doorn <jvdoorn@antarc.com>
2022-01-21 16:56:13 +0100

    Improved version of window shift effect --- untested.

--- measure/schemes/window_shift_effect.py
+++ measure/schemes/window_shift_effect.py
@@ -16,65 +16,61 @@ WINDOW_SIZE = 12
 REGION_SIZE = 20
 ITERATIONS = 2 * 4 * REGION_SIZE
 
-CORRECTION_FACTOR = 0
+CA_INDEX = 0
+WA_INDEX = 1
+CB_INDEX = 2
+WB_INDEX = 3
+C1_INDEX = 4
+C2_INDEX = 5
+CO_INDEX = 6
 
 
 class WindowShiftEffect(BaseScheme):
     def __init__(self, *args, shift_A: bool = True, **kwargs):
-        super().__init__(*args, data_points=5, iterations=ITERATIONS, **kwargs)
-
+        super().__init__(*args, data_points=7, iterations=ITERATIONS, **kwargs)
         self.shift_A = shift_A
-        if shift_A:
-            self.shift_line_C = DelayLines.CA
-            self.shift_line_W = DelayLines.WA
-            self.fixed_line_C = DelayLines.CB
-            self.fixed_line_W = DelayLines.WB
-        else:
-            self.shift_line_C = DelayLines.CB
-            self.shift_line_W = DelayLines.WB
-            self.fixed_line_C = DelayLines.CA
-            self.fixed_line_W = DelayLines.WA
 
-        # Middle of the achievable delay range
         fixed_delay = LOWER_DELAY_LIMIT + REGION_SIZE
-        self.fixed_delay_C = self.fixed_line_C.calculate_steps(fixed_delay)
-        self.fixed_delay_W = self.fixed_line_W.calculate_steps(fixed_delay + WINDOW_SIZE)
-
-        # Add a correction so that the windows are centered around 0.
-        correction = CORRECTION_FACTOR if shift_A else -CORRECTION_FACTOR
-        fixed_delay -= correction
-
         start_delay = fixed_delay - REGION_SIZE
         end_delay = fixed_delay + REGION_SIZE
+
         desired_delays = np.linspace(start_delay, end_delay, self._iterations)
 
-        # Optimal steps for the delays.
-        self.data[0] = self.shift_line_C.calculate_steps(desired_delays)
-        self.data[1] = self.shift_line_W.calculate_steps(desired_delays + WINDOW_SIZE)
+        if shift_A:
+            self.data[CA_INDEX, :] = DelayLines.CA.calculate_steps(desired_delays)
+            self.data[WA_INDEX, :] = DelayLines.WA.calculate_steps(fixed_delay + WINDOW_SIZE)
+            self.data[CB_INDEX, :] = np.tile(DelayLines.CB.calculate_steps(fixed_delay), self._iterations)
+            self.data[WB_INDEX, :] = np.tile(DelayLines.WB.calculate_steps(fixed_delay + WINDOW_SIZE), self._iterations)
+        else:
+            self.data[CA_INDEX, :] = np.tile(DelayLines.CA.calculate_steps(fixed_delay), self._iterations)
+            self.data[WA_INDEX, :] = np.tile(DelayLines.WA.calculate_steps(fixed_delay + WINDOW_SIZE), self._iterations)
+            self.data[CB_INDEX, :] = DelayLines.CB.calculate_steps(desired_delays)
+            self.data[WB_INDEX, :] = DelayLines.WB.calculate_steps(fixed_delay + WINDOW_SIZE)
 
     @property
     def metadata(self) -> dict:
         metadata = super().metadata
         metadata.update({
-            'window_size':       WINDOW_SIZE,
-            'region_size':       REGION_SIZE,
-            'shift_A':           self.shift_A,
-            'fixed_delay_C':     self.fixed_delay_C,
-            'correction_factor': CORRECTION_FACTOR,
+            'window_size': WINDOW_SIZE,
+            'region_size': REGION_SIZE,
+            'shift_A':     self.shift_A,
         })
         return metadata
 
     def setup(self):
-        self.coincidence_circuit.set_delay(self.fixed_delay_C, self.fixed_line_C)
-        self.coincidence_circuit.set_delay(self.fixed_delay_W, self.fixed_line_W)
+        pass
 
     def iteration(self, i):
         # Set the desired state
-        delay_C = self.data[0, i]
-        delay_W = self.data[1, i]
-        self.coincidence_circuit.set_delay(delay_C, self.shift_line_C)
-        self.coincidence_circuit.set_delay(delay_W, self.shift_line_W)
-        self.data[2:, i] = self.coincidence_circuit.measure(MEASURE_TIME)
+        self.coincidence_circuit.set_delay(self.data[CA_INDEX, i], DelayLines.CA)
+        self.coincidence_circuit.set_delay(self.data[WA_INDEX, i], DelayLines.WA)
+        self.coincidence_circuit.set_delay(self.data[CB_INDEX, i], DelayLines.CB)
+        self.coincidence_circuit.set_delay(self.data[WB_INDEX, i], DelayLines.WB)
+
+        counts1, counts2, coincidences = self.coincidence_circuit.measure(MEASURE_TIME)
+        self.data[C1_INDEX, i] = counts1
+        self.data[C2_INDEX, i] = counts2
+        self.data[CO_INDEX, i] = coincidences
 
     @classmethod
     def _plot_counts(cls, delay, counts1, counts2, coincidences, popt, metadata):
@@ -120,17 +116,16 @@ class WindowShiftEffect(BaseScheme):
 
     @classmethod
     def analyse(cls, data, metadata):
-        shift_line_C = DelayLines.CA if metadata['shift_A'] else DelayLines.CB
-        fixed_line_C = DelayLines.CB if metadata['shift_A'] else DelayLines.CA
+        if metadata['shift_A']:
+            delay = DelayLines.CA.calculate_delays(data[CA_INDEX, :]) - DelayLines.CB.calculate_delays(
+                data[CB_INDEX, :])
+        else:
+            delay = DelayLines.CB.calculate_delays(data[CB_INDEX, :]) - DelayLines.CA.calculate_delays(
+                data[CA_INDEX, :])
 
-        fixed_delay = fixed_line_C.calculate_delays(metadata['fixed_delay_C'])
-        delay = shift_line_C.calculate_delays(data[0, :]) - fixed_delay
-        correction = metadata['correction_factor'] if metadata['shift_A'] else -metadata['correction_factor']
-        delay += correction
-
-        counts1 = data[2, :]
-        counts2 = data[3, :]
-        coincidences = data[4, :]
+        counts1 = data[C1_INDEX, :]
+        counts2 = data[C2_INDEX, :]
+        coincidences = data[CO_INDEX, :]
 
         p0 = (0, 1, 2, 150, 15000)
         popt, _ = curve_fit(cls._distribution, delay, coincidences, p0=p0)
