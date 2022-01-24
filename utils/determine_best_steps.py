2061fa6cd82e13a31749b484e63a9273e1160b9d
DouweRem <remmeltsdouwe@gmail.com>
2022-01-24 09:57:20 +0100

    Added program to determine the best step values from a WindowShift measurement

---
+++ utils/determine_best_steps.py
@@ -0,0 +1,28 @@
+import numpy as np
+from utils.delays import DelayLines
+
+# Load the last/best file of the WindowShift
+file = '/Users/douweremmelts/PycharmProjects/interface_new/data/WindowShiftEffect/2022-01-18-10:04:02.npz'
+
+a = np.load(file)
+data = a['data']
+
+index_max = np.argmax(data[4])
+
+if a['shift_A']:
+    shift_C = 'CA'
+    shift_W = 'WA'
+    fixed_C = 'CB'
+    fixed_W = 'WB'
+else:
+    shift_C = 'CB'
+    shift_W = 'WB'
+    fixed_C = 'CA'
+    fixed_W = 'WA'
+
+print(f'Max coincidence counts at index = {index_max}')
+print(f'Optimal steps for {shift_C} = {data[0, index_max]}')
+print(f'Optimal steps for {shift_W} = {data[1, index_max]}')
+print(f'Optimal steps for {fixed_C} = {a["fixed_delay_C"]}')
+print(f'Optimal steps for {fixed_W} ='
+      f' {DelayLines.WB.calculate_steps(DelayLines.CB.calculate_delays(a["fixed_delay_C"])+ a["window_size"])}')
